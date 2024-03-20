bb310a6001fbd6bfbf7e65b8ec8f56b1b0cc4ce3
RafatMeraz <razhossain201@gmail.com>
2024-03-20 23:41:33 +0600

    Add Basic widgets - Scaffold | Column | Row | Dialog | Snackbar

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,4 +1,4 @@
-// MaterialApp | Scaffold | Text | Center | Image | Icon
+// Scaffold | Column | Row | Dialog | bottom sheet | Snackbar
 
 import 'package:flutter/material.dart';
 
@@ -11,38 +11,81 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hello World',
+      // debugShowCheckedModeBanner: false,
       home: Home(),
     );
   }
 }
+
+// Column => Vertically
+// Row => Horizontally
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Write whatever you want to show in screen
     return Scaffold(
-      // backgroundColor: Colors.grey,
+      backgroundColor: Colors.lightGreen.shade50,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Home screen', style: TextStyle(
+          color: Colors.white
+        ),),
+        leading: Icon(Icons.home_filled, color: Colors.white,),
+        actions: [
+          IconButton(onPressed: () {
+            // ScaffoldMessenger.of(context).showSnackBar(
+            //   SnackBar(
+            //     content: Text('Money has been transferred'),
+            //     backgroundColor: Colors.green,
+            //     duration: Duration(seconds: 1),
+            //   ),
+            // );
+            showDialog(context: context, builder: (context) {
+              return AlertDialog(
+                title: Text('Send money'),
+                content: Text('Are you sure that you want to send money?'),
+                actions: [
+
+                ],
+              );
+            });
+          }, icon: Icon(Icons.add)),
+        ],
+      ),
       body: Center(
-        // child: Text(
-        //   'Hello World. We are starting a new application from today askflsk flaksf lkslf k;slkf;lks lfksdlkf l;sklf sl;fk l;s;l',
-        //   textAlign: TextAlign.center,
-        //   maxLines: 1,
-        //   style: TextStyle(
-        //     fontSize: 24,
-        //     fontWeight: FontWeight.w600,
-        //     backgroundColor: Colors.purple,
-        //     color: Colors.white,
-        //     overflow: TextOverflow.ellipsis
-        //   ),
-        // ),
-        // child: Icon(Icons.android, size: 84, color: Colors.green,),
-        // child: Image.network(
-        //   'https://picsum.photos/seed/picsum/536/354',
-        //   width: 200,
-        //   height: 300,
-        //   fit: BoxFit.scaleDown,
-        // ),
-        // child: Image.asset('images/panda.jpg', width: 300, height: 300, fit: BoxFit.scaleDown, ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Hello'),
+            Text('World from Rafat'),
+            Text('From'),
+            Text('Ostad'),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text('Greeting from '),
+                Text('Ovi'),
+                Text('-'),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.date_range),
+                        Text('Date'),],
+                    ),
+                    Text('12-12-12'),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }

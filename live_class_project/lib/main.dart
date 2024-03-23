f1a28d89b628919065fc01136a8d98fd76c15e92
RafatMeraz <razhossain201@gmail.com>
2024-03-23 23:47:47 +0600

    Add Basic widgets - Buttons(ElevatedButton, TextButton, IconButton, GestureDetector, InkWell) |  TextField | Container | Padding

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,4 +1,5 @@
-// Scaffold | Column | Row | Dialog | bottom sheet | Snackbar
+// Buttons(ElevatedButton, TextButton, IconButton, GestureDetector, InkWell)
+// |  TextField | Container | Padding
 
 import 'package:flutter/material.dart';
 
@@ -59,31 +60,196 @@ class Home extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Hello'),
-            Text('World from Rafat'),
-            Text('From'),
-            Text('Ostad'),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  side: BorderSide(
+                    width: 2,
+                    color: Colors.green
+                  )
+                ),
+                textStyle: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16)
+              ),
+              onPressed: () {
+                print('Pressed');
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.add),
+                  Text('Press here'),
+                ],
+              ),
+            ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Text('Greeting from '),
-                Text('Ovi'),
-                Text('-'),
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.date_range),
-                        Text('Date'),],
-                    ),
-                    Text('12-12-12'),
-                  ],
-                )
-              ],
-            )
+            TextButton(
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.green,
+                  textStyle: TextStyle(fontSize: 24),
+              ),
+              onPressed: () {},
+              child: Text(
+                'TextButton',
+              ),
+            ),
+
+            IconButton(
+              onPressed: () {
+                print('Tapped in icon button');
+              },
+              icon: Icon(
+                Icons.add_circle,
+                color: Colors.deepOrangeAccent,
+              ),
+            ),
+
+            GestureDetector(
+              onTap: () {
+                print('on tap detected');
+              },
+              onDoubleTap: () {
+                print('Double tap detected');
+              },
+              onLongPress: () {
+                print('Long pressed');
+              },
+              child: Column(
+                children: [
+                  Text('sdfsdfsd'),
+                  Icon(Icons.add_circle),
+                  Row(
+                    children: [
+                      Text('This is a row')
+                    ],
+                  )
+                ],
+              ),
+            ),
+
+            InkWell(
+              splashColor: Colors.orange,
+              radius: 20,
+              borderRadius: BorderRadius.circular(30),
+              onTap: () {
+                print('on tap detected');
+              },
+              onDoubleTap: () {
+                print('Double tap detected');
+              },
+              onLongPress: () {
+                print('Long pressed');
+              },
+              child: Column(
+                children: [
+                  Text('sdfsdfsd'),
+                  Icon(Icons.add_circle),
+                  Row(
+                    children: [
+                      Text('This is a row')
+                    ],
+                  )
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: TextField(
+                // controller: TextEditingController(),
+                // enabled: false,
+                onChanged: (value) {
+                  print(value);
+                },
+                style: TextStyle(fontSize: 24, color: Colors.orange),
+                maxLength: 10,
+                decoration: InputDecoration(
+                  hintText: 'Enter your email',
+                  hintStyle: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  // label: Text('Email'),
+                  labelText: 'Email',
+                  labelStyle: TextStyle(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(width: 4, color: Colors.red)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 4, color: Colors.orange),
+                    borderRadius: BorderRadius.circular(16)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(width: 4, color: Colors.red)
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(width: 4, color: Colors.grey)
+                  ),
+                  fillColor: Colors.white,
+                  filled: true,
+                  prefixIcon: Icon(Icons.add),
+                  prefixIconColor: Colors.green,
+                  // prefix: Column(
+                  //   children: [
+                  //     Icon(Icons.add),
+                  //   ],
+                  // ),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  suffixIconColor: Colors.red
+                  // suffix: Column(
+                  //   children: [],
+                  // )
+                ),
+              ),
+            ),
+
+          /*  Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(24),
+                border: Border.all(color: Colors.black, width: 2),
+                // border: Border(
+                //   top: BorderSide(color: Colors.black, width: 3),
+                //   bottom: BorderSide(color: Colors.black, width: 2),
+                //   left: BorderSide(color: Colors.black, width: 2),
+                //   right: BorderSide(color: Colors.black, width: 2),
+                // ),
+                image: DecorationImage(
+                  image: AssetImage('images/panda.jpg'),
+                  fit: BoxFit.cover
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.9),
+                    spreadRadius: 5,
+                    blurRadius: 8,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                  // BoxShadow(
+                  //   color: Colors.pink.withOpacity(0.9),
+                  //   spreadRadius: 5,
+                  //   blurRadius: 8,
+                  //   offset: Offset(0, 3), // changes position of shadow
+                  // ),
+                ]
+              ),
+              alignment: Alignment.center,
+              child: Text('Panda', style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+              ),),
+            )*/
           ],
         ),
       ),

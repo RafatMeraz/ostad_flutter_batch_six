ffa649fb0ca3fe0c2f47b2d4a153b502200d6357
Rafat Meraz <razhossain201@gmail.com>
2024-03-28 00:08:22 +0600

    Add Basic Widget - ListView, Gridview

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -22,6 +22,14 @@ class MyApp extends StatelessWidget {
 // Row => Horizontally
 
 class Home extends StatelessWidget {
+  List<String> studentList = [
+    'Jahid',
+    'Nayeem',
+    'Abir',
+    'Hasan',
+    'Tonu'
+  ];
+
   @override
   Widget build(BuildContext context) {
     // Write whatever you want to show in screen
@@ -54,202 +62,214 @@ class Home extends StatelessWidget {
           }, icon: Icon(Icons.add)),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      // body: SingleChildScrollView(
+      //   child: Column(
+      //     children: [
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //       Text('sdfjdkslfkds'),
+      //     ]
+      //   ),
+      // )
+      /*body: ListView(
+          scrollDirection: Axis.horizontal,
           children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.orange,
-                foregroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  side: BorderSide(
-                    width: 2,
-                    color: Colors.green
-                  )
-                ),
-                textStyle: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16)
-              ),
-              onPressed: () {
-                print('Pressed');
-              },
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.add),
-                  Text('Press here'),
-                ],
-              ),
-            ),
-
-            TextButton(
-              style: TextButton.styleFrom(
-                  foregroundColor: Colors.green,
-                  textStyle: TextStyle(fontSize: 24),
-              ),
-              onPressed: () {},
-              child: Text(
-                'TextButton',
-              ),
-            ),
-
-            IconButton(
-              onPressed: () {
-                print('Tapped in icon button');
-              },
-              icon: Icon(
-                Icons.add_circle,
-                color: Colors.deepOrangeAccent,
-              ),
-            ),
-
-            GestureDetector(
-              onTap: () {
-                print('on tap detected');
-              },
-              onDoubleTap: () {
-                print('Double tap detected');
-              },
-              onLongPress: () {
-                print('Long pressed');
-              },
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+        Text('sdfjdkslfkds'),
+      ]),*/
+      /*body: ListView.builder(
+        itemCount: 100,
+          itemBuilder: (context, index) {
+            return Text('${index + 1}', style: TextStyle(
+              fontSize: 32
+            ),);
+          }
+      ),*/
+      /*body: ListView.builder(
+          itemCount: studentList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
-                  Text('sdfsdfsd'),
-                  Icon(Icons.add_circle),
-                  Row(
-                    children: [
-                      Text('This is a row')
-                    ],
-                  )
+                  Text(studentList[index], style: TextStyle(
+                    fontSize: 18
+                  ),),
+                  Divider()
                 ],
               ),
-            ),
-
-            InkWell(
-              splashColor: Colors.orange,
-              radius: 20,
-              borderRadius: BorderRadius.circular(30),
-              onTap: () {
-                print('on tap detected');
-              },
-              onDoubleTap: () {
-                print('Double tap detected');
-              },
-              onLongPress: () {
-                print('Long pressed');
-              },
-              child: Column(
-                children: [
-                  Text('sdfsdfsd'),
-                  Icon(Icons.add_circle),
-                  Row(
-                    children: [
-                      Text('This is a row')
-                    ],
-                  )
-                ],
-              ),
-            ),
-
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: TextField(
-                // controller: TextEditingController(),
-                // enabled: false,
-                onChanged: (value) {
-                  print(value);
-                },
-                style: TextStyle(fontSize: 24, color: Colors.orange),
-                maxLength: 10,
-                decoration: InputDecoration(
-                  hintText: 'Enter your email',
-                  hintStyle: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  // label: Text('Email'),
-                  labelText: 'Email',
-                  labelStyle: TextStyle(),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(width: 4, color: Colors.red)
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 4, color: Colors.orange),
-                    borderRadius: BorderRadius.circular(16)
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(width: 4, color: Colors.red)
-                  ),
-                  disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(width: 4, color: Colors.grey)
-                  ),
-                  fillColor: Colors.white,
-                  filled: true,
-                  prefixIcon: Icon(Icons.add),
-                  prefixIconColor: Colors.green,
-                  // prefix: Column(
-                  //   children: [
-                  //     Icon(Icons.add),
-                  //   ],
-                  // ),
-                  suffixIcon: Icon(Icons.remove_red_eye),
-                  suffixIconColor: Colors.red
-                  // suffix: Column(
-                  //   children: [],
-                  // )
-                ),
-              ),
-            ),
-
-          /*  Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.circular(24),
-                border: Border.all(color: Colors.black, width: 2),
-                // border: Border(
-                //   top: BorderSide(color: Colors.black, width: 3),
-                //   bottom: BorderSide(color: Colors.black, width: 2),
-                //   left: BorderSide(color: Colors.black, width: 2),
-                //   right: BorderSide(color: Colors.black, width: 2),
-                // ),
-                image: DecorationImage(
-                  image: AssetImage('images/panda.jpg'),
-                  fit: BoxFit.cover
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.9),
-                    spreadRadius: 5,
-                    blurRadius: 8,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                  // BoxShadow(
-                  //   color: Colors.pink.withOpacity(0.9),
-                  //   spreadRadius: 5,
-                  //   blurRadius: 8,
-                  //   offset: Offset(0, 3), // changes position of shadow
-                  // ),
-                ]
-              ),
-              alignment: Alignment.center,
-              child: Text('Panda', style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold
+            );
+          },
+      ),*/
+      /*body: ListView.separated(
+        itemCount: studentList.length,
+        itemBuilder: (context, index) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(studentList[index],
+                style: TextStyle(
+                  fontSize: 18
               ),),
-            )*/
+              Text(studentList[index],
+                style: TextStyle(
+                  fontSize: 18
+              ),),
+            ],
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider(
+            height: 32,
+            thickness: 2,
+            endIndent: 16,
+            indent: 8,
+            color: Colors.pink,
+          );
+        },
+      ),*/
+      /*body: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 6),
+        children: [
+          Text('1'),
+          Text('2'),
+          Text('3'),
+          Text('4'),
+          Text('5'),
+          Text('6'),
+          Text('7'),
+          Text('8'),
+          Text('8'),
+        ],
+      ),*/
+      // ALT + ENTER (Win)
+      // opt + Enter (Mac)
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              primary: false,
+              itemCount: studentList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        studentList[index],
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Divider()
+                    ],
+                  ),
+                );
+              },
+            ),
+            GridView.builder(
+              shrinkWrap: true,
+              primary: false,
+              itemCount: 100,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, childAspectRatio: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10
+                ),
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Text('Roll - ${index+1}'),
+                      // Text(studentList[index])
+                    ],
+                  );
+                },),
           ],
         ),
       ),

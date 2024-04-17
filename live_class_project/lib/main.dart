cf0498adff679c17132bc188c5f5db2c03acad18
RafatMeraz <razhossain201@gmail.com>
2024-04-17 23:28:18 +0600

    Add Aspect Ratio, Expanded, FractionallySizedBox

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,3 +1,5 @@
+// Aspect Ratio | Expanded | FractionallySizedBox
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,71 +23,55 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // MediaQuery
-    print(MediaQuery.of(context).size);
-    print(MediaQuery.of(context).size.width);
-    print(MediaQuery.of(context).size.height);
-    print(MediaQuery.of(context).orientation);
-    print(MediaQuery.of(context).devicePixelRatio);
-    print(MediaQuery.of(context).displayFeatures);
-
-    print(MediaQuery.displayFeaturesOf(context));
-    print(MediaQuery.sizeOf(context));
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: const Text('Home'),
       ),
-      // body: Center(
-      //   child: Wrap(
-      //     alignment: WrapAlignment.center,
-      //     crossAxisAlignment: WrapCrossAlignment.start,
-      //     spacing: 4,
-      //     children: [
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //       Text(MediaQuery.orientationOf(context).toString()),
-      //     ],
-      //   ),
-      // ),
-     /* body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          if (constraints.maxWidth < 400) {
-            return const Center(child: Text('Mobile'));
-          } else if (constraints.maxWidth < 600) {
-            return const Center(child: Text('Tablet'));
-          } else if (constraints.maxWidth < 1200) {
-            return const Center(child: Text('Laptop'));
-          }
-
-          return const Center(child: Text('Desktop'));
-        },
-      ),*/
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          if (orientation == Orientation.landscape) {
-            return Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors.yellow,
-            );
-          } else {
-            return Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors.red,
-            );
-          }
-        },
+      body: Column(
+        children: [
+          // Flexible(
+          //   fit: FlexFit.tight,
+          //   flex: 2,
+          //   child: Container(
+          //     width: 100,
+          //     color: Colors.red,
+          //   ),
+          // ),
+          // Expanded(
+          //   flex: 2,
+          //   child: Container(
+          //     width: 100,
+          //     color: Colors.yellow,
+          //   ),
+          // ),
+          // Expanded(
+          //   flex: 5,
+          //   child: Container(
+          //     width: 100,
+          //     height: 100,
+          //     color: Colors.green,
+          //   ),
+          // ),
+          // SizedBox(
+          //   width: MediaQuery.sizeOf(context).width,
+          //   height: 500,
+          //   child: FractionallySizedBox(
+          //     heightFactor: 0.9,
+          //     widthFactor: 0.8,
+          //     child: Container(
+          //       color: Colors.red,
+          //     ),
+          //   ),
+          // )
+          // 1:2
+          AspectRatio(aspectRatio: 16/5, child: Container(color: Colors.red,),)
+        ],
       ),
     );
   }
 }
+
+
+
+

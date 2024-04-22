a62c1d53a11ad69defae9a79c818e7eae5ce13b6
RafatMeraz <razhossain201@gmail.com>
2024-04-22 23:22:20 +0600

    Add device preview package

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,9 +1,13 @@
-// Aspect Ratio | Expanded | FractionallySizedBox
+import 'package:flutter/foundation.dart';
+import 'package:device_preview/device_preview.dart';
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: kDebugMode,
+    builder: (context) => const MyApp(), // Wrap your app
+  ),);
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       home: const Home(),
       theme: ThemeData(),
     );

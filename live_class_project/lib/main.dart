7687475591de748f0984ef9cacfbcc29ef13de34
RafatMeraz <razhossain201@gmail.com>
2024-08-12 23:13:05 +0600

    Add firebase push notification service

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:live_class_project/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:live_class_project/firebase_notification_service.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -8,5 +9,6 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseNotificationService.instance.initialize();
   runApp(const TodoApp());
 }
--- live_class_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ live_class_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import cloud_firestore
 import firebase_core
+import firebase_messaging
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }

d647a1a932688242c667cc55c91e85aa841bf462
RafatMeraz <razhossain201@gmail.com>
2024-08-19 21:29:33 +0600

    Add static google map

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:live_class_project/app.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:live_class_project/firebase_notification_service.dart';
-import 'firebase_options.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await FirebaseNotificationService.instance.initialize();
-  runApp(const TodoApp());
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
--- live_class_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ live_class_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import cloud_firestore
-import firebase_core
-import firebase_messaging
-import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }

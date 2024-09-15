ac7d3331921c35d3f4bc898e595cfc3808fd8447
RafatMeraz <razhossain201@gmail.com>
2024-09-15 23:07:27 +0600

    Add cart list ui design

--- crafty_bay/lib/presentation/ui/screens/cart_screen.dart
+++ crafty_bay/lib/presentation/ui/screens/cart_screen.dart
@@ -1,4 +1,8 @@
+import 'package:crafty_bay/presentation/state_holders/bottom_nav_bar_controller.dart';
+import 'package:crafty_bay/presentation/ui/utils/app_colors.dart';
+import 'package:crafty_bay/presentation/ui/widgets/cart_item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -10,8 +14,72 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Cart screen'),
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (value) {
+        backToHome();
+      },
+      child: Scaffold(
+        backgroundColor: Colors.grey.shade100,
+        appBar: AppBar(
+          title: const Text('Cart'),
+          leading: IconButton(
+            onPressed: backToHome,
+            icon: const Icon(Icons.arrow_back_ios),
+          ),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return const CartItemWidget();
+                },
+              ),
+            ),
+            _buildPriceAndAddToCartSection()
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget _buildPriceAndAddToCartSection() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          color: AppColors.themeColor.withOpacity(0.1),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(8),
+            topRight: Radius.circular(8),
+          )),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Total Price'),
+              Text(
+                '\$10000',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.themeColor),
+              )
+            ],
+          ),
+          SizedBox(
+              width: 140,
+              child: ElevatedButton(
+                  onPressed: () {}, child: const Text('Checkout')))
+        ],
+      ),
+    );
+  }
+
+  void backToHome() {
+    Get.find<BottomNavBarController>().backToHome();
   }
 }
---
+++ crafty_bay/lib/presentation/ui/widgets/cart_item_widget.dart
@@ -0,0 +1,104 @@
+import 'package:crafty_bay/presentation/ui/utils/app_colors.dart';
+import 'package:crafty_bay/presentation/ui/utils/assets_path.dart';
+import 'package:flutter/material.dart';
+import 'package:item_count_number_button/item_count_number_button.dart';
+
+class CartItemWidget extends StatelessWidget {
+  const CartItemWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Card(
+      elevation: 1,
+      color: Colors.white,
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      child: Row(
+        children: [
+          _buildProductImage(),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Title of product',
+                            style: textTheme.bodyLarge,
+                          ),
+                          _buildColorAndSize(textTheme),
+                        ],
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.delete,
+                        color: Colors.grey,
+                      ),
+                    )
+                  ],
+                ),
+                _buildPriceAndCounter(textTheme)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildColorAndSize(TextTheme textTheme) {
+    return Wrap(
+      spacing: 8,
+      children: [
+        Text(
+          'Color: Red',
+          style: textTheme.bodySmall?.copyWith(color: Colors.grey),
+        ),
+        Text(
+          'Size: XL',
+          style: textTheme.bodySmall?.copyWith(color: Colors.grey),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPriceAndCounter(TextTheme textTheme) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '\$100',
+          style: textTheme.titleMedium?.copyWith(color: AppColors.themeColor),
+        ),
+        ItemCount(
+          initialValue: 1,
+          minValue: 1,
+          maxValue: 20,
+          decimalPlaces: 0,
+          color: AppColors.themeColor,
+          onChanged: (value) {},
+        ),
+      ],
+    );
+  }
+
+  Widget _buildProductImage() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Image.asset(
+        AssetsPath.dummyProductImg,
+        height: 80,
+        width: 80,
+        fit: BoxFit.scaleDown,
+      ),
+    );
+  }
+}
